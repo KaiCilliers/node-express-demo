23cb9dcdff22819015a1fea328a4d17a8f63dec0
Kai Laptop <kaijohan428@gmail.com>
2019-07-21 14:35:55 +0200

    moved home route to a seperate folder using same logic

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const config = require('config');
-const courses = require('./courses');
+const courses = require('./routes/courses');
+const home = require('./routes/home');
 const debug = require('debug')('app:startup');
 
 const app = express();
@@ -28,6 +29,7 @@ if(app.get('env') === 'development') {
 }
 // Syntax: (path, router object that we imported)
 app.use('/api/courses', courses);
+app.use('/', home);
 
 /**
  * Configuration
@@ -35,13 +37,6 @@ app.use('/api/courses', courses);
 debug(`Application Name: ${config.get('name')}`);
 debug(`Application Name: ${config.get('mail.host')}`);
 debug(`Application Name: ${config.get('mail.password')}`);
-
-/**
- * GET
- */
-app.get('/', (req, res) => {
-    res.send('Hello!');
-});
 
 /**
  * Start listening
